364c3ac3652bb485c95342eab70ee93358e36e33
Eduardo Castro <Eduardo Castro>
2015-12-26 03:28:05 -0200

    Start Project

---
+++ js/main.js
@@ -0,0 +1,10 @@
+(function($) {
+    
+     $(".scroll").click(function (event) {
+        event.preventDefault();
+        var idElemento = $(this).attr("href");
+        var deslocamento = $(idElemento).offset().top;
+        $('html, body').animate({ scrollTop: deslocamento }, 'slow');
+    });
+
+})(jQuery);
